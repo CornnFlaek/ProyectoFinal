dcc8ad7be9ecbad4215c60cd9a943f92199796ce
Sofía Beltrán Martín <sofiabelmar@outlook.com>
2019-11-28 18:08:20 -0700

    hertz y microfono

--- ProyectoFinal/Juego.xaml.cs
+++ ProyectoFinal/Juego.xaml.cs
@@ -15,6 +15,10 @@ using System.Windows.Shapes;
 using System.Threading;
 using System.Diagnostics;
 
+using NAudio;
+using NAudio.Wave;
+using NAudio.Dsp;
+
 
 namespace ProyectoFinal
 {
@@ -23,7 +27,9 @@ namespace ProyectoFinal
     /// </summary>
     public partial class Juego : UserControl
     {
-        public bool jugando;
+        WaveIn waveIn; //Conexion con microfono
+        WaveFormat formato; //Formato de audio
+        public bool jugando = true;
         Pepper pepper;
         Stopwatch stopwatch = new Stopwatch();
         TimeSpan tiempoAnterior;
@@ -77,9 +83,7 @@ namespace ProyectoFinal
         private void canvasPrincipal_KeyDown(object sender, KeyEventArgs e)
         {
             
-
-            if (!e.IsRepeat)
-            {
+          
                 if (e.Key == Key.Left)
                 {
                     pepper.CambiarDireccion(Pepper.Direccion.Izquierda);
@@ -97,8 +101,104 @@ namespace ProyectoFinal
                 {
                     pepper.CambiarDireccion(Pepper.Direccion.Abajo);
                 }
+            
+        }
+
+        private void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            jugando = true;
+            //Inicializar la conexion
+            waveIn = new WaveIn();
+
+            //Establecer el formato
+            waveIn.WaveFormat =
+                new WaveFormat(44100, 16, 1);
+            formato = waveIn.WaveFormat;
+
+            //Duracion del buffer
+            waveIn.BufferMilliseconds = 500;
+
+            //Con que funcion respondemos
+            //cuando se llena el buffer
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+
+            waveIn.StartRecording();
+        }
+        private void WaveIn_DataAvailable(object sender,
+            WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesGrabados = e.BytesRecorded;
+
+            int numMuestras = bytesGrabados / 2;
+
+            int exponente = 0;
+            int numeroBits = 0;
+
+            do
+            {
+                exponente++;
+                numeroBits = (int)
+                    Math.Pow(2, exponente);
+            } while (numeroBits < numMuestras);
+            exponente -= 1;
+            numeroBits = (int)
+                Math.Pow(2, exponente);
+            Complex[] muestrasComplejas =
+                new Complex[numeroBits];
+
+            for (int i = 0; i < bytesGrabados; i += 2)
+            {
+                short muestra =
+                    (short)(buffer[i + 1] << 8 | buffer[i]);
+                float muestra32bits =
+                    (float)muestra / 32768.0f;
+                if (i / 2 < numeroBits)
+                {
+                    muestrasComplejas[i / 2].X = muestra32bits;
+                }
+
             }
 
+            FastFourierTransform.FFT(true,
+                exponente, muestrasComplejas);
+
+            float[] valoresAbsolutos =
+                new float[muestrasComplejas.Length];
+
+            for (int i = 0; i < muestrasComplejas.Length;
+                i++)
+            {
+                valoresAbsolutos[i] = (float)
+                    Math.Sqrt(
+                    (muestrasComplejas[i].X * muestrasComplejas[i].X) +
+                    (muestrasComplejas[i].Y * muestrasComplejas[i].Y));
+
+            }
+
+            var mitadValoresAbsolutos =
+                valoresAbsolutos.Take(valoresAbsolutos.Length / 2).ToList();
+
+            int indiceValorMaximo =
+                mitadValoresAbsolutos.IndexOf(
+                mitadValoresAbsolutos.Max());
+
+            float frecuenciaFundamental =
+               (float)(indiceValorMaximo * formato.SampleRate)
+               / (float)valoresAbsolutos.Length;
+
+            lblHertz.Text =
+                frecuenciaFundamental.ToString("n") +
+                " Hz";
+
+
+
+
+        }
+        private void btnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            jugando = false;
+            waveIn.StopRecording();
         }
     }
 }
